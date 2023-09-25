1551454db4926c6a15fdd378fc1c6c043a37c254
Cenriquez23 <cenriquez596@gmail.com>
2023-09-25 16:34:19 -0400

    Complete qualified assignment, js-dev-final-capstone-starter-whack-a-mole.

--- src/index.js
+++ src/index.js
@@ -213,7 +213,7 @@ function starOrSpriteRandomizer(){
 */
 
 //Updates the score
-function updateScore() {
+function updateScore(points) {
   points += 1; 
   score.textContent = points;
   return points;
@@ -279,14 +279,14 @@ function startTimer() {
 function whack(event) {
   // TODO: Write your code here.
   // call updateScore()
-    updateScore("star");
+    updateScore();
     console.log("whacked");
      return points;
   }
 
 //Whacks a sprite
 function whackSprite(event) {
-  updateScore("sprite");
+  updateScore();
   console.log("sprite whacked");
   return points;
 }
